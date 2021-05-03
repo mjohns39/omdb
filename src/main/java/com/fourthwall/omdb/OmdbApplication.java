238750511fd74be6248cacb070dd20f3d6898e0f
michaeljohnson <mjohns39@mix.wvu.edu>
2021-05-02 23:23:24 -0400

    swagger

--- src/main/java/com/fourthwall/omdb/OmdbApplication.java
+++ src/main/java/com/fourthwall/omdb/OmdbApplication.java
@@ -2,6 +2,7 @@ package com.fourthwall.omdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class OmdbApplication {
--- src/main/java/com/fourthwall/omdb/config/WebSecurityConfig.java
+++ src/main/java/com/fourthwall/omdb/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package com.fourthwall.omdb.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,12 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**/*",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .and()
                 .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers("/movies/**/*").permitAll()
                 .antMatchers("/users/**/*").permitAll()
                 .antMatchers("/criticRatings/**/*").permitAll()
@@ -25,6 +34,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
         ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(AUTH_WHITELIST);
     }
 }
 
