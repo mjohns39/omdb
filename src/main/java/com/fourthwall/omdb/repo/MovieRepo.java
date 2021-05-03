70d4689625c73a12bfeb59c3fe068c509e1fe13d
michaeljohnson <mjohns39@mix.wvu.edu>
2021-05-02 20:44:58 -0400

    rest endpoints for searching according to gist requirements plus lookup movie by actor

--- src/main/java/com/fourthwall/omdb/repo/MovieRepo.java
+++ src/main/java/com/fourthwall/omdb/repo/MovieRepo.java
@@ -2,6 +2,13 @@ package com.fourthwall.omdb.repo;
 
 import com.fourthwall.omdb.domain.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MovieRepo extends JpaRepository<Movie, Integer> {
+
+    List<Movie> findByTitle(@Param("title") String title);
+
+    List<Movie> findByActorsLike(@Param("actor") String actor);
 }
--- src/main/java/com/fourthwall/omdb/repo/MovieShowtimeRepo.java
+++ src/main/java/com/fourthwall/omdb/repo/MovieShowtimeRepo.java
@@ -2,6 +2,11 @@ package com.fourthwall.omdb.repo;
 
 import com.fourthwall.omdb.domain.MovieShowtime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MovieShowtimeRepo extends JpaRepository<MovieShowtime, Integer> {
+
+    List<MovieShowtime> findByMovieId(@Param("movieId") Integer movieId);
 }
